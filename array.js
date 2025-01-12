f158feb1bf34abfb1d3ca8cbba098fcc6fb85cbe
NokibReja <nokibreja414@gmail.com>
2025-01-12 19:35:22 +0600

    array

---
+++ array.js
@@ -0,0 +1,17 @@
+const numbers =[10, 20 ,30 ,40 ,50];
+numbers.push(60, 70)
+console.log(numbers)
+
+
+const friends = ["ashik", 'masuk', 'hasib', 'rafi']
+friends.pop()
+console.log(friends)
+
+friends.shift()
+console.log(friends)
+
+const something = [11, 12 ,'kafi', 'sami']
+const out1= something.pop()
+
+console.log(something)
+console.log(out1)
