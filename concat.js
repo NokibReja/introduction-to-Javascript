40cffeaef4c855aaf1603380a15d1ffa0a713be0
NokibReja <nokibreja414@gmail.com>
2025-01-15 23:10:56 +0600

    con

--- concat.js
+++ concat.js
@@ -5,4 +5,5 @@ const fullName = first.concat(' ').concat(last)
 console.log(fullName)
 
 console.log(last.includes('j')) // output: true;
+console.log(last.includes('reja')) // output: true;
 console.log(last.includes('o')) // output: false;
