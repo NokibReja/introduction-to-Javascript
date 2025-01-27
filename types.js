389416edd5cc804891564d3b9495df67c9b7daad
NokibReja <nokibreja414@gmail.com>
2025-01-27 23:27:04 +0600

    type

--- types.js
+++ types.js
@@ -1,9 +1,10 @@
-var name = 'mr. dot';
+var name1 = 'mr. dot';
+var name2 = 'mr. x';
 var id = 123;
 var cls = 11;
 var isRight = true;
 
-console.log (typeof name);
+console.log (typeof name1);
 console.log (typeof id);
 console.log (typeof cls);
 console.log (typeof isRight);
